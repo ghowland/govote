27360cf000da5d9e9a2b71cb91e041060a149650
Geoff Howland <geoff@gmail.com>
2017-05-26 18:03:26 -0700

    Getting initial opsdb importing working, testing with data

--- sql_backups/clean.sql
+++ sql_backups/clean.sql
@@ -1,4 +1,9 @@
-DELETE FROM ip_v4_network;
-DELETE FROM ip_v4_address;
-DELETE FROM location;
+DELETE FROM domain_name CASCADE;
+DELETE FROM ip_v4_network CASCADE;
+DELETE FROM ip_v4_address CASCADE;
+DELETE FROM location CASCADE;
+DELETE FROM machine CASCADE;
+DELETE FROM machine_port_media CASCADE;
+DELETE FROM operating_system_instance CASCADE;
+DELETE FROM operating_system_instance_address CASCADE;
 
