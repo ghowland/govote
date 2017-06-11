fcb5eee6fd575c077d57121688c5f60724bad1ee
Geoff Howland <geoff@gmail.com>
2017-06-10 21:38:51 -0700

    Cleanup

--- govote.go
+++ govote.go
@@ -1115,134 +1115,13 @@ func dynamePage_RenderWidgets(db_web *sql.DB, db *sql.DB, web_site map[string]in
 			page_map[key.(string)] = item.String
 
 		} else if site_page_widget["web_widget_instance_id"] != nil {
-
+			// Render the Widget Instance
 			RenderWidgetInstance(db_web, udn_schema, udn_data, site_page_widget)
-
-			/*
-			// Get the web_widget_instance data
-			sql = fmt.Sprintf("SELECT * FROM web_widget_instance WHERE id = %d", site_page_widget["web_widget_instance_id"])
-			web_widget_instance := Query(db_web, sql)[0]
-
-			fmt.Printf("Web Widget Instance: %s\n", web_widget_instance["name"])
-
-			// We are rendering a Web Widget Instance here instead, load the data necessary for the Processing UDN
-			// Data for the widget instance goes here (Inputs: data, columns, rows, etc.  These are set from the Processing UDN
-			udn_data["widget_instance"] = make(map[string]interface{})
-			// Widgets go here (ex: base, row, row_column, header).  We set this here, below.
-			udn_data["widget"] = make(map[string]interface{})
-
-			// Set web_widget_instance output location (where the Instance's UDN will string append the output)
-			udn_data["widget_instance"].(map[string]interface{})["output_location"] = site_page_widget["web_widget_instance_output"]
-
-			// Get any static content associated with this page widget.  Then we dont need to worry about quoting or other stuff
-			widget_static := make(map[string]interface{})
-			udn_data["static_instance"] = widget_static
-			if site_page_widget["static_data_json"] != nil {
-				err = json.Unmarshal([]byte(web_widget_instance["static_data_json"].(string)), &widget_static)
-				if err != nil {
-					log.Panic(err)
-				}
-			}
-
-			// Get all the web widgets, by their web_widget_instance_widget.name
-			sql = fmt.Sprintf("SELECT * FROM web_widget_instance_widget WHERE web_widget_instance_id = %d", site_page_widget["web_widget_instance_id"])
-			web_instance_widgets := Query(db_web, sql)
-			for _, widget := range web_instance_widgets {
-				sql = fmt.Sprintf("SELECT * FROM web_widget WHERE id = %d", widget["web_widget_id"])
-				web_widgets := Query(db_web, sql)
-				web_widget := web_widgets[0]
-
-				udn_data["widget"].(map[string]interface{})[widget["name"].(string)] = web_widget["html"]
-			}
-
-			// Processing UDN: which updates the data pool at udn_data
-			if site_page_widget["udn_data_json"] != nil {
-				ProcessSchemaUDNSet(db_web, udn_schema, site_page_widget["udn_data_json"].(string), &udn_data)
-			} else {
-				fmt.Printf("UDN Execution: %s: None\n\n", site_page_widget["name"])
-			}
-
-			// We have prepared the data, we can now execute the Widget Instance UDN, which will string append the output to udn_data["widget_instance"]["output_location"] when done
-			if web_widget_instance["udn_data_json"] != nil {
-				ProcessSchemaUDNSet(db_web, udn_schema, web_widget_instance["udn_data_json"].(string), &udn_data)
-			} else {
-				fmt.Printf("Widget Instance UDN Execution: %s: None\n\n", site_page_widget["name"])
-			}
-			*/
 
 		} else if site_page_widget["web_data_widget_instance_id"] != nil {
-			//TODO(g): Make the Widget Instance rendering a separate function, since we have 2 paths to it now...
-			//
 
+			// Render the Widget Instance, from the web_data_widget_instance
 			RenderWidgetInstance(db_web, udn_schema, udn_data, site_page_widget)
-
-			/*
-			// Get the web_data_widget_instance data
-			sql = fmt.Sprintf("SELECT * FROM web_data_widget_instance WHERE id = %d", site_page_widget["web_data_widget_instance_id"])
-			web_data_widget_instance := Query(db_web, sql)[0]
-
-			fmt.Printf("Web Data Widget Instance: %s\n", web_data_widget_instance["name"])
-
-			// Get any static content associated with this page widget.  Then we dont need to worry about quoting or other stuff
-			widget_static = make(map[string]interface{})
-			udn_data["widget_static"] = widget_static
-			if web_data_widget_instance["static_data_json"] != nil {
-				err = json.Unmarshal([]byte(web_data_widget_instance["static_data_json"].(string)), &widget_static)
-				if err != nil {
-					log.Panic(err)
-				}
-			}
-
-			// Get the web_widget_instance data
-			sql = fmt.Sprintf("SELECT * FROM web_widget_instance WHERE id = %d", web_data_widget_instance["web_widget_instance_id"])
-			web_widget_instance := Query(db_web, sql)[0]
-
-			fmt.Printf("Web Widget Instance: %s\n", web_widget_instance["name"])
-
-			// We are rendering a Web Widget Instance here instead, load the data necessary for the Processing UDN
-			// Data for the widget instance goes here (Inputs: data, columns, rows, etc.  These are set from the Processing UDN
-			udn_data["widget_instance"] = make(map[string]interface{})
-			// Widgets go here (ex: base, row, row_column, header).  We set this here, below.
-			udn_data["widget"] = make(map[string]interface{})
-
-			// Set web_widget_instance output location (where the Instance's UDN will string append the output)
-			udn_data["widget_instance"].(map[string]interface{})["output_location"] = site_page_widget["web_widget_instance_output"]
-
-			// Get any static content associated with this page widget.  Then we dont need to worry about quoting or other stuff
-			widget_static := make(map[string]interface{})
-			udn_data["static_instance"] = widget_static
-			if web_widget_instance["static_data_json"] != nil {
-				err = json.Unmarshal([]byte(web_widget_instance["static_data_json"].(string)), &widget_static)
-				if err != nil {
-					log.Panic(err)
-				}
-			}
-
-			// Get all the web widgets, by their web_widget_instance_widget.name
-			sql = fmt.Sprintf("SELECT * FROM web_widget_instance_widget WHERE web_widget_instance_id = %d", web_data_widget_instance["web_widget_instance_id"])
-			web_instance_widgets := Query(db_web, sql)
-			for _, widget := range web_instance_widgets {
-				sql = fmt.Sprintf("SELECT * FROM web_widget WHERE id = %d", widget["web_widget_id"])
-				web_widgets := Query(db_web, sql)
-				web_widget := web_widgets[0]
-
-				udn_data["widget"].(map[string]interface{})[widget["name"].(string)] = web_widget["html"]
-			}
-
-			// Processing UDN: which updates the data pool at udn_data
-			if web_data_widget_instance["udn_data_json"] != nil {
-				ProcessSchemaUDNSet(db_web, udn_schema, web_data_widget_instance["udn_data_json"].(string), &udn_data)
-			} else {
-				fmt.Printf("UDN Execution: %s: None\n\n", site_page_widget["name"])
-			}
-
-			// We have prepared the data, we can now execute the Widget Instance UDN, which will string append the output to udn_data["widget_instance"]["output_location"] when done
-			if web_widget_instance["udn_data_json"] != nil {
-				ProcessSchemaUDNSet(db_web, udn_schema, web_widget_instance["udn_data_json"].(string), &udn_data)
-			} else {
-				fmt.Printf("Widget Instance UDN Execution: %s: None\n\n", site_page_widget["name"])
-			}
-			*/
 
 		} else {
 			panic("No web_widget_id, web_widget_instance_id, web_data_widget_instance_id.  Site Page Widgets need at least one of these.")
